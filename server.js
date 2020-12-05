8247073a3f860f27698fa8d7d0906116d25e02fa
Kinshuk chadha <kinshukchadha@gmail.com>
2020-12-05 11:04:26 -0400

    added functionality to register and created packages for passport

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 //require express
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const users = [];
 
 app.set('view-engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req,res) =>{
     res.render('index.ejs' , {name: 'kinshuk'});
@@ -12,12 +17,33 @@ app.get('/login', (req,res) =>{
     res.render('login.ejs');
 });
 
+app.post('/login', (req,res) =>{
+
+});
+
 app.get('/register', (req,res) =>{
     res.render('register.ejs');
 });
 
-app.post('/register', (req,res) =>{
+app.post('/register', async (req,res) =>{
 
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+            users.push({
+                id: Date.now.toString(),
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword
+            });
+
+        res.redirect('/login');
+
+        }
+    catch{
+        res.redirect('/register');
+    }
+    console.log(users);
 });
 
 //listen on this port
